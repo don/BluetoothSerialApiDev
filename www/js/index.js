e2c740cf2dae2fe545c434227eff62cc9ea67b94
Don Coleman <dcoleman@chariotsolutions.com>
2015-03-02 00:13:44 -0500

    reformat

--- www/js/index.js
+++ www/js/index.js
@@ -1,7 +1,7 @@
 var log = function(message) {
-  console.log(message);
-  outputDiv.innerHTML = message;
-};
+        console.log(message);
+        outputDiv.innerHTML = message;
+    };
 
 var app = {
     initialize: function() {
@@ -34,232 +34,228 @@ var app = {
 
     },
     onDeviceReady: function() {
-      log("Hello World");
+        log("Hello World");
     },
     list: function() {
 
-      function success(peripherals) {
-        log(JSON.stringify(peripherals));
-        // populate mac address for first device
-        connectText.value = peripherals[0].id;
-      }
+        function success(peripherals) {
+            log(JSON.stringify(peripherals));
+            // populate mac address for first device
+            connectText.value = peripherals[0].id;
+        }
 
-      function failure(reason) {
-        log(reason || "Listing peripherals failed");
-      }
+        function failure(reason) {
+            log(reason || "Listing peripherals failed");
+        }
 
-      bluetoothSerial.list(success, failure);
+        bluetoothSerial.list(success, failure);
     },
     connect: function() {
 
-      function success() {
-        log("Connected");
-      }
+        function success() {
+            log("Connected");
+        }
 
-      function failure(reason) {
-        log("Connection failed " + reason);
-      }
+        function failure(reason) {
+            log("Connection failed " + reason);
+        }
 
-      bluetoothSerial.connect(connectText.value, success, failure);
+        bluetoothSerial.connect(connectText.value, success, failure);
     },
     disconnect: function() {
 
-      function success() {
-        log("Disconnected");
-      }
+        function success() {
+            log("Disconnected");
+        }
 
-      function failure(reason) {
-        log("Disconnect failed " + reason);
-      }
+        function failure(reason) {
+            log("Disconnect failed " + reason);
+        }
 
-      bluetoothSerial.disconnect(success, failure);
+        bluetoothSerial.disconnect(success, failure);
     },
     available: function() {
 
-      function success(bytesAvailable) {
-        log(bytesAvailable);
-      }
+        function success(bytesAvailable) {
+            log(bytesAvailable);
+        }
 
-      function failure(reason) {
-        log("Available failed " + reason);
-      }
+        function failure(reason) {
+            log("Available failed " + reason);
+        }
 
-      bluetoothSerial.available(success, failure);
+        bluetoothSerial.available(success, failure);
     },
     read: function() {
 
-      function success(data) {
-        log(data);
-      }
+        function success(data) {
+            log(data);
+        }
 
-      function failure(reason) {
-        log("Read failed " + reason);
-      }
+        function failure(reason) {
+            log("Read failed " + reason);
+        }
 
-      bluetoothSerial.read(success, failure);
+        bluetoothSerial.read(success, failure);
     },
     readUntil: function() {
 
-      function success(data) {
-        log(data);
-      }
+        function success(data) {
+            log(data);
+        }
 
-      function failure(reason) {
-        log("Read until failed " + reason);
-      }
+        function failure(reason) {
+            log("Read until failed " + reason);
+        }
 
-      var delimiter = readUntilText.value;
-      if (delimiter === "\\n") {
-        delimiter = "\n";
-      }
+        var delimiter = readUntilText.value;
+        if (delimiter === "\\n") {
+            delimiter = "\n";
+        }
 
-      bluetoothSerial.readUntil(delimiter, success, failure);
+        bluetoothSerial.readUntil(delimiter, success, failure);
 
     },
     clear: function() {
 
-      function success() {
-        log("Cleared buffer");
-      }
+        function success() {
+            log("Cleared buffer");
+        }
 
-      function failure(reason) {
-        log("Clear failed " + reason);
-      }
+        function failure(reason) {
+            log("Clear failed " + reason);
+        }
 
-      bluetoothSerial.clear(success, failure);
+        bluetoothSerial.clear(success, failure);
     },
     subscribe: function() {
 
-      function success(data) {
-        log(data);
-      }
+        function success(data) {
+            log(data);
+        }
 
-      function failure(reason) {
-        log("Subscribe failed " + reason);
-      }
+        function failure(reason) {
+            log("Subscribe failed " + reason);
+        }
 
-      var delimiter = subscribeText.value;
-      if (delimiter === "\\n") {
-        delimiter = "\n";
-      }
+        var delimiter = subscribeText.value;
+        if (delimiter === "\\n") {
+            delimiter = "\n";
+        }
 
-      bluetoothSerial.subscribe(delimiter, success, failure);
+        bluetoothSerial.subscribe(delimiter, success, failure);
     },
     unsubscribe: function() {
 
-      function success() {
-        log("Unsubscribed");
-      }
+        function success() {
+            log("Unsubscribed");
+        }
 
-      function failure(reason) {
-        log("Unsubscribe failed " + reason);
-      }
+        function failure(reason) {
+            log("Unsubscribe failed " + reason);
+        }
 
-      bluetoothSerial.unsubscribe(success, failure);
+        bluetoothSerial.unsubscribe(success, failure);
 
     },
     subscribeRawData: function() {
 
-      function success(buffer) {
-        log(JSON.stringify(new Uint8Array(buffer)));
-        //log(buffer);
-      }
+        function success(buffer) {
+            log(JSON.stringify(new Uint8Array(buffer)));
+            //log(buffer);
+        }
 
-      function failure(reason) {
-        log("Subscribe Raw Data failed " + reason);
-      }
+        function failure(reason) {
+            log("Subscribe Raw Data failed " + reason);
+        }
 
-      bluetoothSerial.subscribeRawData(success, failure);
+        bluetoothSerial.subscribeRawData(success, failure);
 
     },
     unsubscribeRawData: function() {
 
-      function success() {
-        log("Unsubscribed Raw Data");
-      }
+        function success() {
+            log("Unsubscribed Raw Data");
+        }
 
-      function failure(reason) {
-        log("Unsubscribe Raw Data failed " + reason);
-      }
+        function failure(reason) {
+            log("Unsubscribe Raw Data failed " + reason);
+        }
 
-      bluetoothSerial.unsubscribeRawData(success, failure);
+        bluetoothSerial.unsubscribeRawData(success, failure);
 
     },
     write: function() {
 
-      function success() {
-        log("OK");
-      }
+        function success() {
+            log("OK");
+        }
 
-      function failure(reason) {
-        log("Write failed " + reason);
-      }
+        function failure(reason) {
+            log("Write failed " + reason);
+        }
 
-      var message = writeText.value;
+        var message = writeText.value;
 
-      bluetoothSerial.write(message, success, failure);
+        bluetoothSerial.write(message, success, failure);
 
     },
     writeBytes: function() {
 
-      function success() {
-        log("OK");
-      }
+        function success() {
+            log("OK");
+        }
 
-      function failure(reason) {
-        log("Write failed " + reason);
-      }
+        function failure(reason) {
+            log("Write failed " + reason);
+        }
 
-      var message = writeBytesText.value;
-      // assume string is space delimited octets e.g FF 00 AA
-      var parts = message.split(" ");
-      var array = new Uint8Array(parts.length);
-      for (var i = 0; i < parts.length; i++) {
-        array[i] = parseInt(parts[i], 16);
-      }
+        var message = writeBytesText.value;
+        // assume string is space delimited octets e.g FF 00 AA
+        var parts = message.split(" ");
+        var array = new Uint8Array(parts.length);
+        for (var i = 0; i < parts.length; i++) {
+            array[i] = parseInt(parts[i], 16);
+        }
 
-      bluetoothSerial.write(array, success, failure);
+        bluetoothSerial.write(array, success, failure);
 
     },
     isEnabled: function() {
-      bluetoothSerial.isEnabled(
+        bluetoothSerial.isEnabled(
+
         function() {
-          log("Bluetooth is enabled");
-        },
-        function() {
-          log("Bluetooth is *not* enabled");
-        }
-      );
+            log("Bluetooth is enabled");
+        }, function() {
+            log("Bluetooth is *not* enabled");
+        });
     },
     isConnected: function() {
-      bluetoothSerial.isConnected(
+        bluetoothSerial.isConnected(
+
         function() {
-          log("Bluetooth is connected");
-        },
-        function() {
-          log("Bluetooth is *not* connected");
-        }
-      );
+            log("Bluetooth is connected");
+        }, function() {
+            log("Bluetooth is *not* connected");
+        });
     },
     showBluetoothSettings: function() {
-      bluetoothSerial.showBluetoothSettings(
+        bluetoothSerial.showBluetoothSettings(
+
         function() {
-          log("Bluetooth settings should have appeared");
-        },
-        function() {
-          log("Problem showing Bluetooth settings");
-        }
-      );
+            log("Bluetooth settings should have appeared");
+        }, function() {
+            log("Problem showing Bluetooth settings");
+        });
     },
     enable: function() {
-      bluetoothSerial.enable(
+        bluetoothSerial.enable(
+
         function() {
-          log("Bluetooth is Enabled");
-        },
-        function(reason) {
-          log(reason);
-        }
-      );
+            log("Bluetooth is Enabled");
+        }, function(reason) {
+            log(reason);
+        });
     }
 };
 
